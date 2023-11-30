7d5c8918e86813ae311129d7131b2106f49f1135
Jonathan Kulak <lackadaize@msn.com>
2023-11-29 19:13:22 -0500

    This is the git commit for the Functions: Continued assignment.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun());
+  console.log(saturdayFun("bathe my dog"));
+  
+  // mondayWork function
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  console.log(mondayWork());
+  console.log(mondayWork("work from home"));
+  
+  // wrapAdjective function
+  function wrapAdjective(asterisk = "*") {
+    return function innerAdjectiveWrapper (descriptive = "a hard worker") {
+      return (`You are ${asterisk}${descriptive}${asterisk}!`)
+    }
+  }
+  
+  console.log(wrapAdjective()());
